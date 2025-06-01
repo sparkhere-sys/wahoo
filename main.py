71a52e4a91eea5948d420022ad794c6e2cfeb2fc
_SparkHere <116731471+sparkhere-sys@users.noreply.github.com>
2025-06-01 19:02:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 ## wahoo!
 ## v0.3 beta
 ## made with <3 by spark
-## certain lines of code will be commented out with ##. thats an intentional decision, aka me being to lazy to hold down the backspace key.
+## certain lines of code will be commented out with ##. thats an intentional decision, aka me being too lazy to hold down the backspace key.
 ## docstrings in v0.4 :)
 
 # LIBRARIES AND MODULES
