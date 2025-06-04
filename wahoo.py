6be4060253eeea56185f16969ac7c18636e1c1f0
_SparkHere <116731471+sparkhere-sys@users.noreply.github.com>
2025-06-04 13:48:47 +0300

    Update wahoo.py

--- wahoo.py
+++ wahoo.py
@@ -48,13 +48,13 @@ def run(cmd, dir=None, yolo=False, exit=False):
       print("wahoo error: Command failed.")
     sys.exit(1)
 
-def install(pkg, source="https://aur.archlinux.org", build=True, segfault=True):
+def install(pkg, source="https://aur.archlinux.org", build=True, segfault=True, yolo=False):
   if pkg == "wahoo" and segfault:
     print("wahoo: Bold of you for trying to install wahoo with wahoo.")
-    os.kill(os.getpid(), 11) # Segmentation fault (core dumped)
+    print("Segmentation fault (core dumped)") # os.kill(os.getpid(), 11)
     
   if not internet_check():
-    print("wahoo error: No internet. Aborting install...")
+    print("wahoo error: No internet. Aborted.")
     sys.exit(1)
   
   wahooroot = Path.home() / ".wahoo" / "source"
@@ -68,8 +68,11 @@ def install(pkg, source="https://aur.archlinux.org", build=True, segfault=True):
       print(f"wahoo: Downloading {pkg} from AUR...")
     else:
       print(f"wahoo: Downloading {pkg}...")
-      
-    run(f"git clone {source}/{pkg}.git", wahooroot, False)
+
+    if yolo:
+      run(f"git clone {source}/{pkg}.git", wahooroot, yolo)
+    else:
+      run(f"git clone {source/{pkg}.git", wahooroot, False)
     print(f"wahoo! {pkg} Downloaded.")
 
   else:
@@ -107,10 +110,10 @@ def uninstall(pkg, yolo=False):
   
   sourcedir = Path.home() / ".wahoo" / "source" / pkg
   
-  run(f"sudo pacman -Rns {pkg} --noconfirm", yolo=False) # the print above was commented out since run() already shows what command is being run, so its redundant to have two of them.
+  run(f"sudo pacman -Rns {pkg} --noconfirm", yolo=yolo) # the print above was commented out since run() already shows what command is being run, so its redundant to have two of them.
   print(f"wahoo! {pkg} uninstalled.")
   print("wahoo: Cleaning up source directory...")
-  run(f"rm -rf {sourcedir}", yolo=False, exit=True)
+  run(f"rm -rf {sourcedir}", yolo=yolo, exit=True)
   print("wahoo! Source directory cleaned.")
   
 
@@ -119,18 +122,15 @@ def help():
   print("[Available commands]")
   print("install, -S:                  Installs a package from the AUR.")
   print("uninstall, remove, -R, -Rns:  Uninstalls an existing package.")
-  print("help, -H:                     Prints this message.")
+  print("help, -H, --help:             Prints this message.")
   print("update, -Sy:                  Updates an existing AUR package. You can also update wahoo with this.")
   print("list, -Q, -Qs                 Shows all packages installed. If used with a second argument, it'll search for packages of the same name on your system.")
   print("info, show, -Qi               Shows info for a specific package.")
+  print("version, --version:           Prints the version of wahoo you're running.")
   print("[Available flags]")
   print("Nothing yet.")
   print("[Usage]")
-  print("wahoo -S foo")
-  print("wahoo install foo")
-  print("wahoo update wahoo")
-  print("wahoo -Sy foo")
-  print("wahoo uninstall foo")
+  print("wahoo <command> <pkg> <flags> (for commands like install, uninstall, etc)")
 
 def flagparsing(flags):
   ## print("wahoo warn: No flag support yet.") # uhhhhh
@@ -154,14 +154,14 @@ def flagparsing(flags):
   return parsed_flags
         
 
-def update(pkg):
+def update(pkg, yolo=False):
   if not internet_check():
-    print("wahoo error: No internet. Aborting...")
+    print("wahoo error: No internet. Aborted.")
     sys.exit(1)
   wahooroot = Path.home() / ".wahoo" / "source" / pkg
 
   if not wahooroot.exists():
-    print(f"wahoo error: {pkg} doesn't exist on your system. Install it with wahoo install {pkg}.") # might make it call install() here since maybe people are using wahoo with the pacman command syntax. if i do that, this will change from a 'wahoo error' to a 'wahoo warn'.
+    print(f"wahoo error: {pkg} doesn't exist on your system. Install it with wahoo install {pkg}.") # might make it call install() here since maybe people are using wahoo with the pacman command syntax. if i do that, this will change from a 'wahoo error' to a 'wahoo warn'. v0.5 maybe.
     return
 
   print(f"wahoo: Starting update")
@@ -171,25 +171,27 @@ def update(pkg):
   except:
     return
 
-  prompt = input(f"wahoo: Rebuild and install {pkg}? [Y/n]").strip().lower()
-  if prompt == "n":
-    print("wahoo: Skipping reinstall.")
-    return
+  if not yolo:
+    prompt = input(f"wahoo: Rebuild and install {pkg}? [Y/n]").strip().lower()
+    if prompt == "n":
+      print("wahoo: Skipping reinstall.")
+      return
 
   try:
     print("wahoo: Starting rebuild...")
     print("wahoo: Removing old package...")
-    uninstall(pkg, True) # runs with yolo
+    uninstall(pkg, True) # runs with yolo regardless of --yolo. bad UX? maybe. but i just want to push this update
     print("wahoo: Building and installing...")
-    run("makepkg -si", wahooroot, True)
+    run("makepkg -si", wahooroot, True) # ditto
     print(f"wahoo! {pkg} updated.")
-  except:
-    return
+  except Exception as e:
+    print(f"wahoo error: Update failed. ({e})")
+    sys.exit(1)
 
 def search(pkg):
   ## print("Nothing here yet...") # coming soon! ...in valve time.
   if not internet_check():
-    print("wahoo error: No internet. Aborting...")
+    print("wahoo error: No internet. Aborted.")
     sys.exit(1)
 
   url = f"https://aur.archlinux.org/rpc/?v=5&type=search&arg={pkg}"
@@ -230,13 +232,13 @@ def main():
         print("wahoo error: No package or invalid package specified.")
         sys.exit(1)
       
-      install(pkg)  
+      install(pkg, yolo=parsed_flags.get("flag_yolo", False))  
     case ("remove" | "uninstall" | "-R" | "-Rns"):
       if not pkg:
         print("wahoo error: No package or invalid package specified.")
         sys.exit(1)
       
-      uninstall(pkg)
+      uninstall(pkg, yolo=parsed_flags.get("flag_yolo", False))
     case ("help" | "-H" | "--help" | "--h"):
       help()
       sys.exit(1)
