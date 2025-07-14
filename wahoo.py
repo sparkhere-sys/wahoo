955ab93743a5df6fb18a4cf22d1240ac751b640b
_SparkHere <116731471+sparkhere-sys@users.noreply.github.com>
2025-07-14 17:29:47 +0300

    Update wahoo.py

--- wahoo.py
+++ wahoo.py
@@ -3,13 +3,14 @@
 wahoo's launcher and the thing that actually gets installed to /usr/bin/ by pacman
 '''
 
-from wahoo.parser import parse
+from wahoo.parser import parse # as main
 import wahoo.cliutils as cli
 from wahoo.constants import *
 
 if __name__ == "__main__":
   try:
-    main()
+    ## main()
+    parse()
   except KeyboardInterrupt:
     cli.echo("Interrupted by Ctrl+C, see ya next time")
     ## sys.exit(130)
